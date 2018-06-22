78756411088e58e57359988aa60c922d848dde2e
ashokmasetty <40477436+ashokmasetty@users.noreply.github.com>
2018-06-22 00:36:41 -0400

    Update IFileIOService.cs

--- HelloWorldInfrastructure/Services/IFileIOService.cs
+++ HelloWorldInfrastructure/Services/IFileIOService.cs
@@ -1,10 +1,3 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="IFileIOService.cs" company="Ryan Woodcox">
-//  Copyright (c) 2015 All Rights Reserved
-//  <author>Ryan Woodcox</author>
-// </copyright>
-//-----------------------------------------------------------------------
-
 namespace HelloWorldInfrastructure.Services
 {
     /// <summary>
