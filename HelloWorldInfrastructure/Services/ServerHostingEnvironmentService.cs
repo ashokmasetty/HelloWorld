4db1e36f701e15f73db1de480aea39c52f71ca3e
ashokmasetty <40477436+ashokmasetty@users.noreply.github.com>
2018-06-22 00:38:03 -0400

    Update ServerHostingEnvironmentService.cs

--- HelloWorldInfrastructure/Services/ServerHostingEnvironmentService.cs
+++ HelloWorldInfrastructure/Services/ServerHostingEnvironmentService.cs
@@ -1,10 +1,3 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="ServerHostingEnvironmentService.cs" company="Ryan Woodcox">
-//  Copyright (c) 2015 All Rights Reserved
-//  <author>Ryan Woodcox</author>
-// </copyright>
-//-----------------------------------------------------------------------
-
 namespace HelloWorldInfrastructure.Services
 {
     using System.Web.Hosting;
