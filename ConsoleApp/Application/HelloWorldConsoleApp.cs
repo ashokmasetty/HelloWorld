9d1fb9a96863d5bfb97387c5b42f512c2dead8d3
ashokmasetty <40477436+ashokmasetty@users.noreply.github.com>
2018-06-22 00:18:45 -0400

    Update HelloWorldConsoleApp.cs

--- ConsoleApp/Application/HelloWorldConsoleApp.cs
+++ ConsoleApp/Application/HelloWorldConsoleApp.cs
@@ -1,10 +1,3 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="HelloWorldConsoleApp.cs" company="Ryan Woodcox">
-//  Copyright (c) 2015 All Rights Reserved
-//  <author>Ryan Woodcox</author>
-// </copyright>
-//-----------------------------------------------------------------------
-
 namespace ConsoleApp.Application
 {
     using ConsoleApp.Services;
